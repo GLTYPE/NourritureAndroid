b97480963ce4c804e0193c2cb68a00213ec66280
JunhanYan <yanjunhan1989@gmail.com>
2014-12-25 20:42:30 +0800

    AndroidApp

--- NourritureAndroidApp/gen/com/gltype/nurriture/R.java
+++ NourritureAndroidApp/gen/com/gltype/nurriture/R.java
@@ -50,22 +50,25 @@ public final class R {
         public static final int usericon=0x7f020016;
     }
     public static final class id {
-        public static final int action_settings=0x7f090017;
-        public static final int btn_writer=0x7f090012;
-        public static final int emailText=0x7f09000c;
-        public static final int et_search=0x7f090011;
-        public static final int imageview=0x7f090014;
+        public static final int action_settings=0x7f09001a;
+        public static final int btn_login=0x7f090010;
+        public static final int btn_writer=0x7f090015;
+        public static final int chbx_rem=0x7f090011;
+        public static final int et_email=0x7f09000d;
+        public static final int et_pass=0x7f09000f;
+        public static final int et_search=0x7f090014;
+        public static final int imageview=0x7f090017;
         public static final int img_item_content_pic=0x7f090006;
         public static final int img_item_content_subpic=0x7f090008;
         public static final int img_item_userphoto=0x7f090002;
         public static final int layout_title_bar=0x7f090001;
-        public static final int loginButton=0x7f09000e;
-        public static final int loginPostButton=0x7f09000f;
-        public static final int lv_moment=0x7f090010;
-        public static final int lv_search=0x7f090013;
-        public static final int passwordText=0x7f09000d;
+        public static final int lv_moment=0x7f090013;
+        public static final int lv_search=0x7f090016;
         public static final int realtabcontent=0x7f090000;
-        public static final int textview=0x7f090015;
+        public static final int textview=0x7f090018;
+        public static final int tv_email=0x7f09000c;
+        public static final int tv_pass=0x7f09000e;
+        public static final int tv_result=0x7f090012;
         public static final int tx_item_time=0x7f090004;
         public static final int txt_item_comment=0x7f09000b;
         public static final int txt_item_content=0x7f090005;
@@ -73,7 +76,7 @@ public final class R {
         public static final int txt_item_redirect=0x7f09000a;
         public static final int txt_item_subcontent=0x7f090007;
         public static final int txt_item_uname=0x7f090003;
-        public static final int txt_title=0x7f090016;
+        public static final int txt_title=0x7f090019;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -91,10 +94,14 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f060001;
         public static final int app_name=0x7f060000;
+        public static final int email=0x7f060007;
         public static final int hello_world=0x7f060002;
+        public static final int input_email=0x7f060003;
         public static final int input_password=0x7f060004;
-        public static final int input_username=0x7f060003;
         public static final int login=0x7f060005;
+        public static final int password=0x7f060008;
+        public static final int remember=0x7f060009;
+        public static final int result=0x7f06000a;
         public static final int search=0x7f060006;
     }
     public static final class style {
--- NourritureAndroidApp/src/com/gltype/nourriture/ui/LoginActivity.java
+++ NourritureAndroidApp/src/com/gltype/nourriture/ui/LoginActivity.java
@@ -1,84 +1,134 @@
 package com.gltype.nourriture.ui;
 
 
-import com.gltype.nourriture.http.AsyncHttpClient;
-import com.gltype.nourriture.http.MyHandler;
-import com.gltype.nourriture.utils.ParamsWrapper;
-import com.gltype.nurriture.R;
+import org.apache.http.Header;
 
-import android.os.Bundle;
-import android.app.Activity;
+import com.gltype.nurriture.R; 
+
+import com.loopj.android.http.AsyncHttpClient;  
+import com.loopj.android.http.AsyncHttpResponseHandler;  
+import com.loopj.android.http.RequestParams;  
+  
+import android.app.Activity;  
 import android.content.Intent;
-import android.view.Menu;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.Toast;
+import android.os.Bundle;  
+import android.text.TextUtils;  
+import android.view.View;  
+import android.widget.EditText;  
+import android.widget.TextView;  
+import android.widget.Toast;  
 
 public class LoginActivity extends Activity {
 	private EditText et_email;
 	private EditText et_password;
+	 private TextView tv_result;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.login);
-		et_email=(EditText) findViewById(R.id.emailText);
-		et_password=(EditText) findViewById(R.id.passwordText);
+		et_email=(EditText) findViewById(R.id.et_email);
+		et_password=(EditText) findViewById(R.id.et_pass);
+		tv_result = (TextView) findViewById(R.id.tv_result); 
 		
 	}
-	public void onClick(View view){
-		String email = et_email.getText().toString().trim();
-		String password = et_password.getText().toString().trim();
-		String url = "http://172.29.64.31:8000/HelloWorld/servlet/LoginServlet?";
-	
-		ParamsWrapper params= new ParamsWrapper();
-		params.put("email", email);
-		params.put("password", password);
-		AsyncHttpClient client = new AsyncHttpClient();
-		client.get(url,params, new MyHandler(){
-			@Override
-			public void onSuccess(String content) {
-				Toast.makeText(LoginActivity.this,content,0).show();
-				super.onSuccess(content);
-			}
-			@Override
-			public void onFailure(String content) {
-				// TODO Auto-generated method stub
-				Toast.makeText(LoginActivity.this,content,0).show();
-				super.onFailure(content);
-			}
-		});
-	}
-	public void onClick_Post(View view){
-		String email = et_email.getText().toString().trim();
-		String password = et_password.getText().toString().trim();
-		String url = "http://172.28.34.137:8000/HelloWorld/servlet/LoginServlet";
-		ParamsWrapper params= new ParamsWrapper();
-		params.put("email", email);
-		params.put("password", password);
-		AsyncHttpClient client = new AsyncHttpClient();
-		client.post(url, params, new MyHandler(){
-			@Override
-			public void onSuccess(String content) {
-				//Toast.makeText(LoginActivity.this,content,0).show();
-				 Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-			     startActivity(intent);
-				super.onSuccess(content);
-			}
-			@Override
-			public void onFailure(String content) {
-				// TODO Auto-generated method stub
-				Toast.makeText(LoginActivity.this,content,0).show();
-				super.onFailure(content);
-			}
-		});
-	}
+	public void login(View v) {  
+       
+        int id = v.getId();  
+       
+        switch (id) {  
+        case R.id.btn_login:  
+           
+            String userName = et_email.getText().toString(); 
+            String userPass = et_password.getText().toString();  
+           
+            if (TextUtils.isEmpty(userName.trim())  
+                    || TextUtils.isEmpty(userPass.trim())) {  
+                Toast.makeText(this, "Email or Password can't be empty", Toast.LENGTH_LONG).show();  
+            } else {  
+                
+                loginByAsyncHttpClientPost(userName, userPass);  
+                //loginByAsyncHttpClientGet(userName, userPass);  
+            }  
+            break;  
+        }  
+    }  
+  
+   
+    public void loginByAsyncHttpClientPost(String userName, String userPass) {  
+        AsyncHttpClient client = new AsyncHttpClient(); 
+        String url = "http://172.29.64.31:8000/HelloWorld/servlet/LoginServlet"; 
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+        RequestParams params = new RequestParams();  
+        params.put("email", userName);  
+        params.put("password", userPass);
+       
+        client.post(url, params, new AsyncHttpResponseHandler() {  
+           
+            @Override  
+            public void onSuccess(int statusCode, Header[] headers,  
+                    byte[] responseBody) {  
+                if (statusCode == 200) {  
+                    //tv_result.setText(new String(responseBody)); 
+                   String resResult = new String(responseBody);
+                    if("Login Success".equals(resResult)){
+                    	Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    	startActivity(intent);
+                    }
+                    else{
+                    	 tv_result.setText("Wrong Email or Password");
+                    }
+                }  
+            }  
+  
+           
+            @Override  
+            public void onFailure(int statusCode, Header[] headers,  
+                    byte[] responseBody, Throwable error) {  
+                error.printStackTrace();
+            }  
+        });  
+    }  
+  
+    
+    public void loginByAsyncHttpClientGet(String userName, String userPass) {  
+      
+        AsyncHttpClient client = new AsyncHttpClient();  
+      
+        String url = "http://172.29.64.31:8000/HelloWorld/servlet/LoginServlet";  
+       
+        RequestParams params = new RequestParams();  
+        params.put("email", userName);
+        params.put("password", userPass);
+          
+       
+        client.get(url, params,new AsyncHttpResponseHandler() {  
+            @Override  
+            public void onSuccess(int statusCode, Header[] headers,  
+                    byte[] responseBody) {  
+               
+                System.out  
+                        .println("statusCode-------------------" + statusCode);  
+               
+                for (int i = 0; i < headers.length; i++) {  
+                    Header header = headers[i];  
+                    System.out.println("header------------Name:"  
+                            + header.getName() + ",--Value:"  
+                            + header.getValue());  
+                }  
+              
+                tv_result.setText(new String(responseBody));  
+            }  
+  
+            @Override  
+            public void onFailure(int statusCode, Header[] headers,  
+                    byte[] responseBody, Throwable error) {  
+               
+                error.printStackTrace();  
+            }  
+        });  
+    }  
+
+	
 
 }
