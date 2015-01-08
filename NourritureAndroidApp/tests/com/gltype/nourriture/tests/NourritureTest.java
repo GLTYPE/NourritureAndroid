5172352fac7354e3433a1c72c9a5696ca4891727
李永楠|Morgan|Léon <14126123@bjtu.edu.cn>
2015-01-09 07:20:58 +0800

    Add Instument to Manifest. Add A package named com.gltype.nourriture.tests

---
+++ NourritureAndroidApp/tests/com/gltype/nourriture/tests/NourritureTest.java
@@ -0,0 +1,11 @@
+package com.gltype.nourriture.tests;
+
+import android.test.AndroidTestCase;
+
+public class NourritureTest extends AndroidTestCase {
+
+	public NourritureTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ NourritureAndroidApp/tests/com/gltype/nourriture/tests/test1.java
@@ -0,0 +1,11 @@
+package com.gltype.nourriture.tests;
+
+import com.android.uiautomator.testrunner.UiAutomatorTestCase;
+
+public class test1 extends UiAutomatorTestCase{
+
+	public test1() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
